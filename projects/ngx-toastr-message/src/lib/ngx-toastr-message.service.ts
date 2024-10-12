a0bffd496f0501607c3a5081de30b563740aee5e
Tharindu37 <tharindut520@gmail.com>
2024-10-12 18:59:41 +0530

    develop basic toastr message

--- projects/ngx-toastr-message/src/lib/ngx-toastr-message.service.ts
+++ projects/ngx-toastr-message/src/lib/ngx-toastr-message.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+export interface ToasterMessage {
+  message: string;
+  type: 'success' | 'error' | 'info' | 'warning';
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NgxToastrMessageService {
+  private messageSubject = new Subject<ToasterMessage>();
+  public messages$ = this.messageSubject.asObservable();
+  constructor() {}
 
-  constructor() { }
+  show(message: string, type: 'success' | 'error' | 'info' | 'warning') {
+    this.messageSubject.next({ message, type });
+  }
 }
